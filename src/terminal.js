40aae55119a01a871e4d1ec39ecc5ac9eea54a7d
alexnicita <alexandernicita@gmail.com>
2025-02-18 10:29:18 -0500

    another attempt at clean mobile fix

--- src/terminal.js
+++ src/terminal.js
@@ -110,6 +110,7 @@ const keyboard = (parse) => {
   return {
     keypress: (ev) => {
       if (key(ev) === 'enter') {
+        input.push('\n');  // Add newline to input first
         const str = input.join('').trim();
         parse(str);
         input = [];
