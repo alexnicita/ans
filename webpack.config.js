0086289df9d6f5a06707f583e72417b6216cf889
alexnicita <alexandernicita@gmail.com>
2025-06-13 10:33:06 -0400

    Update dependencies and webpack config for modern Node.js compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,50 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-const mode = process.env.NODE_ENV || 'development';
-const minimize = mode === 'production';
+const mode = process.env.NODE_ENV || "development";
+const minimize = mode === "production";
 const plugins = [];
 
-if (mode === 'production') {
-  plugins.push(new OptimizeCSSAssetsPlugin({
-    cssProcessorOptions: {
-      discardComments: true
-    },
-  }));
+if (mode === "production") {
+  plugins.push(new CssMinimizerPlugin());
 }
 
 module.exports = {
   mode,
-  devtool: 'source-map',
-  entry: [
-    path.resolve(__dirname, 'index.js'),
-  ],
+  devtool: "source-map",
+  entry: [path.resolve(__dirname, "index.js")],
   output: {
-    sourceMapFilename: '[file].map',
-    filename: '[name].js'
+    sourceMapFilename: "[file].map",
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
   },
   optimization: {
     minimize,
+    minimizer: [new CssMinimizerPlugin()],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html')
+      template: path.resolve(__dirname, "src/index.html"),
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
+      filename: "[name].css",
+      chunkFilename: "[id].css",
     }),
-    ...plugins
+    ...plugins,
   ],
   devServer: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 9001,
-    compress: true
+    compress: true,
   },
   module: {
     rules: [
       {
         test: /\.(eot|woff|ttf|svg)$/,
         exclude: /(node_modules|bower_components)/,
-        use: [
-          {
-            loader: 'file-loader'
-          }
-        ]
+        type: "asset/resource",
       },
       {
         test: /\.(sa|sc|c)ss$/,
@@ -60,36 +52,35 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'postcss-loader',
+            loader: "css-loader",
             options: {
               sourceMap: true,
-              plugins: [
-                require('autoprefixer')()
-              ]
-            }
+            },
           },
           {
-            loader: 'sass-loader',
+            loader: "postcss-loader",
             options: {
-              minimize,
-              sourceMap: true
-            }
-          }
-        ]
+              sourceMap: true,
+              postcssOptions: {
+                plugins: [require("autoprefixer")()],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
       },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
 };
